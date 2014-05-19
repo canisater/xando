1f86d507de91dd2818b112a8c30803f9268103b7
Gary Whittingham <squanto@hotmail.co.uk>
2014-05-19 19:49:21 +0100

    Update Model to check for winner

--- src/model.py
+++ src/model.py
@@ -15,28 +15,40 @@ class Model(object):
     '''
     classdocs
     '''
-    (DRAW, XWIN, YWIN, TO_PLAY) = 0,1,2,3
+    (DRAW, WIN, TO_PLAY) = 0,1,2
     (X, Y) = 0,1  
     
-    lines = ({0,1,2}, {3,4,5}, {6,7,8}, {0,3,6},
-             {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6})
+    lines = ((0,1,2), (3,4,5), (6,7,8), (0,3,6),
+             (1,4,7), (2,5,8), (0,4,8), (2,4,6))
     
-
+    def check_line(self, square, a, b, c, ):
+        return (self.grid[a] == self.grid[b] ==
+                self.grid[c] == self.grid[square])
+            
     def set_square(self, square):
         event = Event()
         event.type = Event.SET_SQUARE
         event.square = square 
-        self.grid[square] = self.state    
-        self.change_state()
+
+        self.grid[square] = self.to_play    
+        
+        for line in lines:
+            if self.check_line(square, line):
+                self.state = Model.WIN    
+
+        if self.grid.count('2') == 0:
+            self.state = Model.DRAW 
+                
         self.notifyObservers(event)
 
     def reset(self):
         self.to_play = (self.to_play + 1) % 2
-        self.xs = set()
-        self.ys = set()
+        self.state = Model.TO_PLAY
         self.grid = [2,2,2,2,2,2,2,2,2]
+
         event = Event()
         event.type = Event.RESET 
+
         self.notifyObservers(event)
 
     def register(self, observer):
@@ -52,8 +64,6 @@ class Model(object):
         '''
         print ("Created model")
         self.observers = []
-        self.state = Model.TO_PLAY
+        
         self.to_play = Model.X
-        self.xs = set()
-        self.ys = set()
         self.grid = [2,2,2,2,2,2,2,2,2]
